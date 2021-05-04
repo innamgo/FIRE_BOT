1ac4e26dc2046f8e4cf7e28e081a5ed45e094ffd
innamgo <Jeju!4844>
2021-05-04 11:01:42 +0900

    all item sell added

--- all_ticker_sell_market_price.py
+++ all_ticker_sell_market_price.py
@@ -40,31 +40,35 @@ def get_element_value(source_list, match_attribute, find_value, return_attribute
     return return_value
 
 def insert_trade_transaction_log(transaction_type, market_code, order_result):
-    if type(order_result) == type(''):
-        insert_trade_transaction_log_query = f"""
-        insert into trade_transaction_log (transaction_type, market_coin, response_json, user_id)
-        values('{transaction_type}', '{market_code}', '{order_result.replace("'",'"')}', '{user_id}')
-        """
-        logger.info(insert_trade_transaction_log_query)
-        cur.execute(insert_trade_transaction_log_query)
-        conn.commit()
-    elif type(order_result) == type([]):
-        for order_item in order_result:
+    try:
+        if type(order_result) == type(''):
             insert_trade_transaction_log_query = f"""
-                    insert into trade_transaction_log (transaction_type, market_coin, response_json, user_id)
-                    values('{transaction_type}', '{market_code}', '{str(order_item[0]).replace("'", '"')}', '{user_id}')
-                    """
+            insert into trade_transaction_log (transaction_type, market_coin, response_json, user_id)
+            values('{transaction_type}', '{market_code}', '{order_result.replace("'",'"').replace("None",'"None"')}', '{user_id}')
+            """
             logger.info(insert_trade_transaction_log_query)
             cur.execute(insert_trade_transaction_log_query)
             conn.commit()
-    else:
-        insert_trade_transaction_log_query = f"""
-        insert into trade_transaction_log (transaction_type, market_coin, response_json, user_id)
-        values('{transaction_type}', '{market_code}', '{str(order_result).replace("'",'"')}', '{user_id}')
-        """
-        logger.info(insert_trade_transaction_log_query)
-        cur.execute(insert_trade_transaction_log_query)
-        conn.commit()
+        elif type(order_result) == type([]):
+            for order_item in order_result:
+                insert_trade_transaction_log_query = f"""
+                        insert into trade_transaction_log (transaction_type, market_coin, response_json, user_id)
+                        values('{transaction_type}', '{market_code}', '{str(order_item[0]).replace("'", '"').replace("None",'"None"')}', '{user_id}')
+                        """
+                logger.info(insert_trade_transaction_log_query)
+                cur.execute(insert_trade_transaction_log_query)
+                conn.commit()
+        else:
+            insert_trade_transaction_log_query = f"""
+            insert into trade_transaction_log (transaction_type, market_coin, response_json, user_id)
+            values('{transaction_type}', '{market_code}', '{str(order_result).replace("'",'"').replace("None",'"None"')}', '{user_id}')
+            """
+            logger.info(insert_trade_transaction_log_query)
+            cur.execute(insert_trade_transaction_log_query)
+            conn.commit()
+    except Exception as ex:
+        logger.error(str(ex))
+    traceback.print_exc()
 try:
     my_balance = upbit.get_balances()
     auto_trade_except = select_auto_trade_except()
@@ -84,19 +88,18 @@ try:
         logger.info('try sell : ' + market_name)
         non_trade_list = upbit.get_order(market_name)
         logger.info(non_trade_list)
-        wait_buy_trade = get_element_include(non_trade_list, 'side', 'ask')  # 매수 미체결 개수
-        wait_sell_trade = get_element_include(non_trade_list, 'side', 'bid')  # 매도 미체결 개수
+        wait_buy_trade = get_element_include(non_trade_list, 'side', 'bid')  # 매수 미체결 개수
+        wait_sell_trade = get_element_include(non_trade_list, 'side', 'ask')  # 매도 미체결 개수
         current_market_balance = upbit.get_balance(ticker=market_name)  # 현재 해당 코인 잔고 조회
         if wait_buy_trade > 0: #매수 미체결 주문이 있으면 취소
-            cancel_uuid = get_element_value(non_trade_list, 'side', 'ask', 'uuid')
-            cancel_order_result = upbit.cancel_order(cancel_uuid)
-            insert_trade_transaction_log('cancel', market_name, cancel_order_result)
-            #sleep(0.1)
-
-        if wait_sell_trade > 0: #매도 미체결 주문이 있으면 취소
             cancel_uuid = get_element_value(non_trade_list, 'side', 'bid', 'uuid')
             cancel_order_result = upbit.cancel_order(cancel_uuid)
-            insert_trade_transaction_log('cancel', market_name, cancel_order_result)
+            insert_trade_transaction_log('cancel_buy', market_name, cancel_order_result)
+
+        if wait_sell_trade > 0: #매도 미체결 주문이 있으면 취소
+            cancel_uuid = get_element_value(non_trade_list, 'side', 'ask', 'uuid')
+            cancel_order_result = upbit.cancel_order(cancel_uuid)
+            insert_trade_transaction_log('cancel_sell', market_name, cancel_order_result)
 
         logger.info('bal:'+ str(current_market_balance))
         sell_order_result = upbit.sell_market_order(market_name, current_market_balance)
